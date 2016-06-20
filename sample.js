847cf80c60c86a6b6312f5ab1b882db95dc73a78
satorukonno <konnosatoru@konnoakira-no-MacBook.local>
2016-06-20 15:23:48 +0900

    add Jump

--- sample.js
+++ sample.js
@@ -1,8 +1,7 @@
 window.onload = function() {
-		// canvasのサイズを指定
-		var canvasWidth = 600, canvasHeight = 400;
-		// シーンの作成
+		var canvasWidth = 600, canvasHeight = 400;// canvasのサイズを指定
 		var scene = new THREE.Scene();
+		// シーンの作成
 		var arrows = {
 			righ:false,
 			left:false,
@@ -10,20 +9,19 @@ window.onload = function() {
 			down:false
 		};
 
-		var player_pos = {
-				x:0,
-				y:0,
-				z:0,
-				size:{x:10,y:10,z:10}
+		var player = {
+				pos:{x:0,y:0,z:0},
+				size:{x:10,y:10,z:10},
+				jump:{flag:false,speed:0}
 		};
 
 		// 箱の作成
-		var boxGeometry = new THREE.BoxGeometry(10, 10, 10);
+		var boxGeometry = new THREE.BoxGeometry(player.size.x, player.size.y, player.size.z);
 		var boxMaterial = new THREE.MeshLambertMaterial({ color: '#E74C3C'});
 		boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
-		boxMesh.position.set(player_pos.x,player_pos.y,player_pos.z);
+		boxMesh.position.set(player.pos.x,player.pos.y,player.pos.z);
 		scene.add(boxMesh);
-
+		console.dir(boxMesh);
 		// カメラの作成
 		var camera = new THREE.PerspectiveCamera(75, canvasWidth / canvasHeight, 1, 1000);
 		camera.position.set(30, 40, 80);
@@ -39,49 +37,54 @@ window.onload = function() {
 		document.body.appendChild(renderer.domElement);
 		renderer.render(scene, camera);
 
-		/*function playerupdate(){
-			if(arrows.up && player_pos.y > 0) { player_pos.y -= 10; }
-			if(arrows.right && player_pos.x+player_pos.size.x < window_size[0]) { player_pos.x += 10; }
-			if(arrows.left && player_pos.x > 0) { player_pos.x -= 10; }
-			if(arrows.down && player_pos.y+player_pos.size.y < window_size[1]) { player_pos.y += 10; }
-			
-			if(player_pos.x >= window_size[0]){
-				player_pos.x -= window_size[0];
+		setInterval(function(){
+				playerupdate();
+				renderer.render(scene, camera);
+		},1000/30)
+		
+		function playerupdate(){
+			if(arrows.up) { player.pos.z -= 1;}	
+			if(arrows.down){ player.pos.z += 1;}
+			if(arrows.left){ player.pos.x -= 1;}
+			if(arrows.right){ player.pos.x += 1;}
+			if(player.pos.y > 0){
+					player.jump.speed += 0.1;
+					player.pos.y -= player.jump.speed*player.jump.speed;
+			}else{
+					player.jump.flag = false;
+					player.pos.y = 0;		
 			}
-			if(player_pos.x < 0){
-				player_pos.x += window_size[0];
-			}
-			if(player_pos.y >= window_size[1]){
-				player_pos.y -= window_size[1];
-			}
-			if(player_pos.y < 0){
-				player_pos.y += window_size[1];
-			}
-			
-		}*/
+			boxMesh.position.set(player.pos.x,player.pos.y,player.pos.z);
+		}
+
+		function Jump(){
+			player.pos.y += 50;
+			player.jump.speed = 0;
+			boxMesh.position.set(player.pos.x,player.pos.y,player.pos.z);
+		}
 
 		document.body.addEventListener('keydown',function (event){
-			// console.log(event.keyCode);
+			console.log(event.keyCode);
 			switch(event.keyCode){
+				case 32 :
+				if(player.pos.y <= 0){
+					Jump();
+				}
+				break
 				case 38 : //UP
 				arrows.up = true;
-				player_pos.y += 10;
 				break;
 				case 40 : //DOWN
 				arrows.down = true;
-				player_pos.y -= 10;
 				break;
 				case 37 : //LEFT
 				arrows.left = true;
-				player_pos.x -= 10;
 				break;
 				case 39 : //RIGHT
 				arrows.right = true;
-				player_pos.x += 10;
 				break;
 			}
-			boxMesh.position.set(player_pos.x,player_pos.y,player_pos.z);
-			renderer.render(scene, camera);
+			//console.log();
 		})
 
 		document.body.addEventListener('keyup',function (event){
@@ -101,4 +104,4 @@ window.onload = function() {
 				break;
 			}
 		})
-};
+}
